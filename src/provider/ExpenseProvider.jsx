4992fe5ed0eda79acde19f09f560c748d71918b6
John Doe <johndoe@email.com>
2025-11-24 19:00:16 +0500

    add_expense

--- src/provider/ExpenseProvider.jsx
+++ src/provider/ExpenseProvider.jsx
@@ -46,7 +46,7 @@ export const ExpenseProvider = ({ children }) => {
 					'Content-Type': '',
 				},
 			})
-			setExpenses(data)
+			setExpenses(data.transactions)
 			return data
 		} catch (error) {
 			const errorMessage = error.response?.data?.error || error.message
